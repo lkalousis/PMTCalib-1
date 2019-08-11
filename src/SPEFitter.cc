2dbb396110b84966089c7c1906790557defdeef2
Leonidas N. Kalousis <leonidas.kalousis@pihc.cnrs.fr>
2019-08-11 10:24:44 +0200

    Fix limits in fitter

--- src/SPEFitter.cc
+++ src/SPEFitter.cc
@@ -61,7 +61,7 @@ double fit_func_mod( const double *x )
   for ( Int_t i=0; i<N; i++ )
     {
       Double_t val = mod0.GetValue( xx0[i] );
-      //if ( val<1.0e-10 ) val = 1.0e-4;
+      if ( val<1.0e-10 ) val = 1.0e-8;
       
       result += pow( val-yy0[i], 2.0 )/( val );
             
@@ -170,12 +170,12 @@ void SPEFitter::FitwDFTmethod( TH1D *hspec )
   
   mFFT->SetFunction(FCA);
 
-  mFFT->SetLimitedVariable( 0, "Norm", dft.Norm, dft.Norm*0.001, dft.Norm*0.5, dft.Norm*2.0 );
+  mFFT->SetLimitedVariable( 0, "Norm", dft.Norm, dft.Norm*0.01, dft.Norm*0.5, dft.Norm*2.0 );
 
   mFFT->SetLimitedVariable( 1, "Q0", dft.Q0, dft.Q0*0.01, dft.Q0*0.9, dft.Q0*1.1 );
   mFFT->SetLimitedVariable( 2, "s0", dft.s0, dft.s0*0.01, dft.s0*0.9, dft.s0*1.1 );
   
-  mFFT->SetLimitedVariable( 3, "mu", dft.mu, 0.01, dft.mu*0.5, dft.mu*1.5 );
+  mFFT->SetLimitedVariable( 3, "mu", dft.mu, 0.01, dft.mu*0.1, dft.mu*2.0 );
   
   mFFT->SetLimitedVariable( 4, "PAR1", dft.spef.params[0], dft.spef.params[0]*0.001, dft.spef.params[0]*0.1, dft.spef.params[0]*5.0 );
   mFFT->SetLimitedVariable( 5, "PAR2", dft.spef.params[1], dft.spef.params[1]*0.01, dft.spef.params[1]*0.1, dft.spef.params[1]*5.0 );
@@ -275,16 +275,16 @@ void SPEFitter::FitwPMTModel( TH1D *hspec )
   
   mMOD->SetFunction(FCA);
 
-  mMOD->SetLimitedVariable( 0, "Norm", mod.params[0], mod.params[0]*0.001, mod.params[0]*0.2, mod.params[0]*5.0 );
-
+  mMOD->SetLimitedVariable( 0, "Norm", mod.params[0], mod.params[0]*0.01, mod.params[0]*0.5, mod.params[0]*2.0 );
+              
   mMOD->SetLimitedVariable( 1, "Q0", mod.params[1], mod.params[1]*0.01, mod.params[1]*0.9, mod.params[1]*1.1 );
   mMOD->SetLimitedVariable( 2, "s0", mod.params[2], mod.params[2]*0.01, mod.params[2]*0.9, mod.params[2]*1.1 );
   
   mMOD->SetLimitedVariable( 3, "mu", mod.params[3], 0.01, mod.params[3]*0.1, mod.params[3]*2.0 );
   
   mMOD->SetLimitedVariable( 4, "PAR1", mod.params[4], mod.params[4]*0.001, mod.params[4]*0.1, mod.params[4]*5.0 );
-  mMOD->SetLimitedVariable( 5, "PAR2", mod.params[5], mod.params[5]*0.01, mod.params[5]*0.1, mod.params[5]*10.0 );
-  mMOD->SetLimitedVariable( 6, "PAR3", mod.params[6], mod.params[6]*0.01, mod.params[6]*0.1, mod.params[6]*10.0 );
+  mMOD->SetLimitedVariable( 5, "PAR2", mod.params[5], mod.params[5]*0.01, mod.params[5]*0.1, mod.params[5]*5.0 );
+  mMOD->SetLimitedVariable( 6, "PAR3", mod.params[6], mod.params[6]*0.01, mod.params[6]*0.05, mod.params[6]*50.0 );
   mMOD->SetLimitedVariable( 7, "PAR4", mod.params[7], 0.01, 0.0, 0.5 );
   
   mMOD->SetMaxFunctionCalls(1.E9);
