693f6889fb9b65d7c46a1cc4fb3c5db82947a484
Leonidas N. Kalousis <leonidas.kalousis@pihc.cnrs.fr>
2019-08-11 10:12:16 +0200

    Fix sg variance

--- mac/example2.C
+++ mac/example2.C
@@ -38,7 +38,7 @@ Int_t example2()
   Pedestal ped( Q0, s0 );
   
   Double_t Q = 40.0;
-  Double_t s = 14.0;
+  Double_t s = 13.0;
   Double_t alpha = 1.0/20.0;
   Double_t w = 0.2;
   Double_t p[4] = { Q, s, alpha, w };
--- mac/example3.C
+++ mac/example3.C
@@ -38,7 +38,7 @@ Int_t example3()
   Pedestal ped( Q0, s0 );
   
   Double_t lambda = 1.0/40.0;
-  Double_t theta = 7.0;
+  Double_t theta = 6.5;
   Double_t alpha = 1.0/20.0;
   Double_t w = 0.2;
   Double_t p[4] = { lambda, theta, alpha, w };
